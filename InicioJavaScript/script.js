d904f13124bc5a8dfe54ae67a90213fc471b14d6
Eric Nathan <ericnathansouza>
2023-08-31 11:36:05 -0300

    js

---
+++ InicioJavaScript/script.js
@@ -0,0 +1,36 @@
+var n1 = 'Eric'
+var n2 = 10
+var soma = null
+var caneca = {
+    nome:'Teu cu',
+    cor: 'colorida',
+    educativa: true
+}
+
+
+var exVetor = []
+
+
+console.log(exVetor)
+console.log(typeof(exVetor))
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alert('Hello Word Eric Neithan')
+
+// prompt('Informe seu nome');
+
+//  confirm('Tem certeza ?')
+// let numero2
+// var numero1
+// const numero3 = 10
