6f162db72a5a6ee939ff0a32329f069c1796ab61
Eric Nathan <ericnathandosouza@gmail.com>
2023-09-18 11:07:06 -0300

    novo commit

---
+++ BancoDeDados/teste/script.js
@@ -0,0 +1,22 @@
+const frutas = ['Abacaxi', 'Morango', 'kiwi', 'goiaba']
+
+
+// frutas.push('maça')//adiciona no final
+// frutas.unshift('uva')//adiciona no inicio
+// frutas.shift()//remove do inicio
+// frutas.pop()//remove do final
+console.log(frutas)
+
+let posicaoArray = frutas.indexOf('kiwi')
+
+itensRemovidosfrutas = frutas.splice(posicaoArray)
+
+console.log(itensRemovidosfrutas)
+
+
+
+var nome = 'Eric'
+console.log(nome)
+console.log(nome.toLowerCase())
+console.log(nome.toUpperCase())
+
